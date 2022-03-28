2110a903886a7be2267ef0445fc27e2d122f7d9a
Fyks <404>
2022-03-28 22:31:39 +0300

    githab

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
-  <div>
-    <h1>My todo's</h1>
-    <Todo text='one' />
-    <Todo text='two' />
-    <Todo text='three' />
-  </div>
+    <div>
+      <h1>My todo's</h1>
+      <Todo text="one" />
+      <Todo text="two" />
+      <Todo text="three" />
+      <Modal />
+      <Backdrop />
+    </div>
   );
 }
 
